cfa6c08ea97406f16dc0e5a32fb54487b769a03f
nsorin <nicolas.sorin1996@gmail.com>
2016-10-23 15:42:21 +0200

    Ajout controllers basiques kernel

---
+++ src/KernelBundle/Form/CountryType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace KernelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CountryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label', 'text');
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+            'data_class' => 'KernelBundle\Entity\Country'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return "";
+    }
+}
---
+++ src/KernelBundle/Form/SettingType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace KernelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SettingType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label', 'text')
+            ->add('value', 'text');
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+            'data_class' => 'KernelBundle\Entity\Setting'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return "";
+    }
+}
